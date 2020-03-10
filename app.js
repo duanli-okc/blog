82a14f02a795d0e2c71557a11c6cb398e4e484fb
duanli <316149574@qq.com>
2020-03-10 09:10:11 +0800

    3th 接口开发

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 //处理http 请求和响应
-const Router=require('./src/router/blog');
+const handleBlogRouter=require('./src/router/blog');
+const handleUserRouter=require('./src/router/user');
 const querystring= require('querystring');
+
 function handleServer (req,res){
-   const method=req.method;
-   req.path= req.url.split('?')[0];
    res.setHeader('Content-type','application/json');
-   req.query=querystring.parse( req.url.split('?')[1] ); 
+   const method=req.method;
+   req.path= req.url.split('?')[0];  // /api/blog/list
+   req.query=querystring.parse( req.url.split('?')[1] );  // { keyword:XXX,id:1}
     
+
+   // 处理博客请求
+   const blogData=handleBlogRouter(req,res);
+   if(blogData){
+      res.end(JSON.stringify(blogData));
+      return;
+   }
+
+   // 处理用户（登陆，注册）
+   const userDate=handleUserRouter(req,res);
+   if(userDate){
+      res.end( JSON.stringify(userDate) );
+      return;
+   }
   
-   // 处理博客列表请求
-   const blogData=Router.handleBlogRouter(req,res);
-   res.end(JSON.stringify(blogData));
+
+
 
 }
 
 module.exports = handleServer;
 
 
-// 业务模块抽离 : 一个模块做一类事  
+  
+
 
 
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,6 +1,7 @@
-// 存在blog路由相关内容
+// 存在blog路由相关内容  
 function handleBlogRouter(req,res){
     const method = req.method;
+   // 请求博客列表
    if(method=='GET' && req.path=='/api/blog/list'){
        // 向数据库获取博客信息  sql
        console.log(req.query); 
@@ -11,8 +12,9 @@ function handleBlogRouter(req,res){
            time:'2353425345'
        }
    } 
+   // 获取博客详情
    if(method=='GET' && req.path=='/api/blog/detail'){
-    // 向数据库获取博客信息  sql
+     // 向数据库获取博客信息  sql
     return {
         id:'1',
         title:'标题1',
@@ -20,8 +22,33 @@ function handleBlogRouter(req,res){
         time:'2353425345'
     }
    }
+   
+   // 新增一篇博客
+   if(method=='POST' && req.path=='/api/blog/new'){
+    // 向数据库获取博客信息  sql
+     return {
+       mes: '新增博客接口'
+     }
+   }
+
+   // 更新一篇博客
+   if(method=='POST' && req.path=='/api/blog/update'){
+    // 向数据库获取博客信息  sql
+     return {
+       mes: '更新博客的接口'
+     }
+   }
+
+   // 删除一篇博客
+   if(method=='POST' && req.path=='/api/blog/del'){
+    // 向数据库获取博客信息  sql
+     return {
+       mes: '删除博客的接口'
+     }
+   }
+
 }
 
-module.exports={
-    handleBlogRouter
-}
+
+
+module.exports=handleBlogRouter
